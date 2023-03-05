b0de23a443f64dbbce26ef0bbd414160fc0a903a
jaredeicm <jaredeicm@gmail.com>
2023-03-05 15:59:59 -0300

    Iniciando repositorio, enviando desafio fibonacci

---
+++ 1. Fibonacci/script.js
@@ -0,0 +1,35 @@
+let result = document.getElementById('result');
+const object = document.getElementById('number');
+
+object.addEventListener('keyup', verifyNumber);
+
+function verifyNumber() {
+  if (object.value > 1) {
+    geraSequencia();
+  } else {
+    result.innerHTML = `A seqûencia sempre inicia com 0 e 1, Insira um número maior que 1`;
+  }
+}
+
+function geraSequencia() {
+  let firstNumber = 0;
+  let nextNumber = 1;
+  let sum = 0;
+  number = object.value;
+  let foundNumber = false;
+  for (let i = 0; i < number; i++) {
+    sum = firstNumber + nextNumber;
+    firstNumber = nextNumber;
+    nextNumber = sum;
+
+    if (sum == number) {
+      foundNumber = true;
+    } else {
+    }
+  }
+  if (foundNumber == true) {
+    result.innerHTML = `Opa! O número ${number} pertence a sequência de Fibonacci`;
+  } else {
+    result.innerHTML = `Huuum , desculpe mas o número ${number} não foi encontrado e não pertence a sequência de Fibonacci`;
+  }
+}
